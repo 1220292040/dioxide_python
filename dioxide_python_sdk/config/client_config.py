fa51b3029ac3bd9a0ee5d78037da4742b763894c
liushangliang <liushanglinag@idea.edu.cn>
2025-10-17 12:32:51 +0800

    feat: update demo

--- dioxide_python_sdk/config/client_config.py
+++ dioxide_python_sdk/config/client_config.py
@@ -1,5 +1,5 @@
 class Config:
-    rpc_url = "http://127.0.0.1:62222/api"
+    rpc_url = "http://127.0.0.1:45678/api"
     log_dir = "logs"
-    ws_rpc = "ws://127.0.0.1:62222/api"
+    ws_rpc = "ws://127.0.0.1:45678/api"
     default_thread_nums = 32
